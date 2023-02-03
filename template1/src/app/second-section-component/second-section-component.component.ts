35d39e80a74b497e1c2704dad457ade608157c69
fernandoaytec <fernando.olmedo@aytecdigital.com>
2023-02-03 14:53:09 +0100

    asd

--- template1/src/app/second-section-component/second-section-component.component.ts
+++ template1/src/app/second-section-component/second-section-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-second-section-component',
@@ -6,9 +6,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecondSectionComponentComponent implements OnInit {
 
+  sm: boolean = false;
+  md: boolean = false;
+  lg:boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+    this.onResize()
   }
-
+  @HostListener("window:resize", [])
+  onResize() {
+    var width = window.innerWidth;
+    if(width<768){
+      console.log("movil "+this.sm)
+      this.sm = true
+      this.md = false
+      this.lg = false
+    }else if(width > 768 && width<1024){
+      this.sm = false
+      this.md = true
+      this.lg = false
+      console.log("es tablet "+this.md)
+    }else{
+      this.sm = false
+      this.md = false
+      this.lg = true
+      console.log("es lg"+this.lg)
+    }
+  }
 }
